9092038943f7b716340533d8505128b39c2f6a27
Vinicius Rio <viniciustrave@gmail.com>
2023-07-31 20:53:05 -0300

    doc(route-config): Save code for router: ...recipe/{2}

--- Core/Router.php
+++ Core/Router.php
@@ -6,16 +6,6 @@ class Router
 {
     public array $routes = [];
 
-    public function add($uri, $handler, $method): static
-    {
-        $this->routes[] = [
-            'uri' => $uri,
-            'handler' => $handler,
-            'method' => $method
-        ];
-
-        return $this;
-    }
     public function route($uri, $method)
     {
         foreach ($this->routes as $route) {
@@ -34,51 +24,14 @@ class Router
         response('Route not found', 404);
     }
 
-//    public function route($uri, $method)
-//    {
-//        foreach ($this->routes as $route) {
-//            $pattern = str_replace('/', '\/', $route['uri']);
-//            $pattern = preg_replace('/{(\w+)}/', '(?<$1>[^\/]+)', $pattern);
-//            $pattern = "/^{$pattern}$/";
-//
-//            if (preg_match($pattern, $uri, $matches) && $route['method'] === strtoupper($method)) {
-//                $handlerInfo = $this->getPathControllerAndAction($route['handler']);
-//                $controllerPath = $handlerInfo['controller'];
-//                $action = $handlerInfo['action'];
-//
-//                if (class_exists($controllerPath)) {
-//                    $controllerInstance = new $controllerPath();
-//                    $params = [];
-//                    foreach ($matches as $key => $value) {
-//                        if (!is_numeric($key)) {
-//                            $params[$key] = $value;
-//                        }
-//                    }
-//
-//                    if (method_exists($controllerInstance, $action)) {
-//                        return $controllerInstance->$action($params['id']);
-//                    }
-//                }
-//            }
-//        }
-//
-//        response('Route not found', 404);
-//    }
-
-
-//    public function getPathControllerAndAction($handler): array
-//    {
-//        $controllerAndAction = explode('@', $handler);
-//        $controllerFolder = $this->getFolderController($controllerAndAction[0]);
-//        $controllerPath = 'Controllers\\' . $controllerFolder . "\\$controllerAndAction[0]";
-//        $action = $controllerAndAction[1];
-//
-//        return [
-//            'controller' => $controllerPath,
-//            'action' => $action,
-//        ];
-//    }
-
+    public function getPathControllerAndAction($handler): array
+    {
+        $controllerAndAction = explode('@', $handler);
+        $controllerFolder = $this->getFolderController($controllerAndAction[0]);
+        $controllerPath = 'Controllers\\' . $controllerFolder . "\\$controllerAndAction[0]";
+        $action = $controllerAndAction[1];
+        return array($controllerPath, $action);
+    }
 
     public function getFolderController($controllerName): string
     {
@@ -97,6 +50,17 @@ class Router
         return $this->add($uri, $handler, 'GET');
     }
 
+    public function add($uri, $handler, $method): static
+    {
+        $this->routes[] = [
+            'uri' => $uri,
+            'handler' => $handler,
+            'method' => $method
+        ];
+
+        return $this;
+    }
+
     public function post($uri, $controller): static
     {
         return $this->add($uri, $controller, 'POST');
@@ -105,15 +69,6 @@ class Router
     public function patch($uri, $controller): static
     {
         return $this->add($uri, $controller, 'PATCH');
-    }
-
-    public function getPathControllerAndAction($handler): array
-    {
-        $controllerAndAction = explode('@', $handler);
-        $controllerFolder = $this->getFolderController($controllerAndAction[0]);
-        $controllerPath = 'Controllers\\' . $controllerFolder . "\\$controllerAndAction[0]";
-        $action = $controllerAndAction[1];
-        return array($controllerPath, $action);
     }
 
 }
