faee715095ee3c202ec0bc504eac4ce10854d6b2
Vinicius Rio <viniciustrave@gmail.com>
2023-07-31 20:54:21 -0300

    feat(routes): Routes in new pattern

--- routes.php
+++ routes.php
@@ -2,8 +2,6 @@
 
 $router->get('/recipes', 'RecipeController@index');
 $router->get('/recipe', 'RecipeController@show');
-
-//$router->get('/recipe/{id}', 'RecipeController@show');
-
-
+$router->patch('/recipes', 'RecipeController@update');
+$router->post('/recipes', 'RecipeController@store');
 
