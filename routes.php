17b70ae4c1a8a56846edec1c57f6109fce2f3d7f
Vinicius Rio <viniciustrave@gmail.com>
2023-07-31 21:04:49 -0300

    feat(routes): Router standard

--- routes.php
+++ routes.php
@@ -4,4 +4,5 @@ $router->get('/recipes', 'RecipeController@index');
 $router->get('/recipe', 'RecipeController@show');
 $router->patch('/recipes', 'RecipeController@update');
 $router->post('/recipes', 'RecipeController@store');
+$router->get('/', 'RecipeController@index');
 
